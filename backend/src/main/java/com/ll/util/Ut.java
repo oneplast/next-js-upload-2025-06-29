e17cb915fbdafc5221cbc7c785a2b494348272f3
Gyeonghun Min <riverpower6@naver.com>
2025-06-30 19:16:53 +0900

    [FEAT] Tika 라이브러리 사용 - MIME 타입 자동 감지

--- backend/src/main/java/com/ll/util/Ut.java
+++ backend/src/main/java/com/ll/util/Ut.java
@@ -133,6 +133,10 @@ public class Ut {
             // 파일 확장자 추출
             String extension = getExtensionFromResponse(response);
 
+            if (extension.equals("tmp")) {
+                extension = getExtensionByTika(tempFilePath);
+            }
+
             // 파일명 추출
             String filename = getFilenameFromUrl(url);
 
@@ -141,6 +145,12 @@ public class Ut {
             mv(tempFilePath, newFilePath);
 
             return newFilePath;
+        }
+
+        public static String getExtensionByTika(String filePath) {
+            String mimeType = AppConfig.getTika().detect(filePath);
+
+            return MIME_TYPE_MAP.getOrDefault(mimeType, "tmp");
         }
 
         @SneakyThrows
--- backend/src/test/java/com/ll/standard/util/FileUtTest.java
+++ backend/src/test/java/com/ll/standard/util/FileUtTest.java
@@ -6,7 +6,11 @@ import com.ll.global.app.AppConfig;
 import com.ll.util.Ut;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@ActiveProfiles("test")
 public class FileUtTest {
     @Test
     @DisplayName("downloadByHttp")
@@ -15,6 +19,17 @@ public class FileUtTest {
 
         // newFilepath 의 확장자가 jpg 인지 확인
         assertThat(newFilePath).endsWith(".jpg");
+
+        Ut.file.delete(newFilePath);
+    }
+
+    @Test
+    @DisplayName("getExtensionByTika")
+    void t2() {
+        String newFilePath = Ut.file.downloadByHttp("https://picsum.photos/id/237/200/300", AppConfig.getTempDirPath());
+
+        String ext = Ut.file.getExtensionByTika(newFilePath);
+        assertThat(ext).isEqualTo("jpg");
 
         Ut.file.delete(newFilePath);
     }
